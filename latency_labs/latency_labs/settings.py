09f90d379ceec965bff63a04b186539ce46530b8
Abdulhabib junis <fatabdi1234@gmail.com>
2025-10-08 14:04:11 +0300

    latency_labs/latency_labs/.git/COMMIT_EDITMSG

--- latency_labs/latency_labs/settings.py
+++ latency_labs/latency_labs/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xur^rn9-_+=1)((cfk6tz$kxfh8-2wa86+6nq$dnni1!cz*y(p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
