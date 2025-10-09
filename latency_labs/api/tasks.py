74cad47457488ea2b358b68e434ffbde93a7a7f5
Abdulhabib junis <fatabdi1234@gmail.com>
2025-10-09 10:00:15 +0300

    tasks.py

---
+++ latency_labs/api/tasks.py
@@ -0,0 +1,19 @@
+# api/tasks.py
+import os
+from celery import Celery
+from django.core.mail import send_mail
+from .models import Device
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'network_monitor.settings')
+
+app = Celery('network_monitor')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+
+@app.task
+def ping_devices():
+    devices = Device.objects.all()
+    for device in devices:
+        # Ping device and check status
+        # Trigger alert if device is down
+        pass
